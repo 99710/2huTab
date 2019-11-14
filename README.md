c4beead2a0cb9a72ae4d9881351380e7cb684cc8
99710 <cuttsharry123@gmail.com>
2019-11-14 10:22:42 +0000

    Initial files

---
+++ README.md
@@ -0,0 +1,7 @@
+# 2huTab
+
+Based on https://github.com/ShantnuS/oof-on-close
+
+Currently a work in progress
+
+
---
+++ main.js
@@ -0,0 +1,31 @@
+var audio;
+
+chrome.tabs.onRemoved.addListener(function(tabid, removed) {
+	if(JSON.parse(localStorage.play_audio)){
+		audio = new Audio('pldead00.wav');
+		//localStorage.playback_rate = 1;
+		//audio.playbackRate = localStorage.playback_rate;
+		audio.volume = 0.3;
+		audio.play();
+	}
+})
+
+chrome.tabs.onCreated.addListener(function(tabid, created) {
+	if(JSON.parse(localStorage.play_audio)){
+		audio = new Audio('extend.wav');
+		//localStorage.playback_rate = 1;
+		//audio.playbackRate = localStorage.playback_rate;
+		audio.volume = 0.3;
+		audio.play();
+	}
+})
+
+chrome.tabs.goBack.addListener(function(tabid, back) {
+	if(JSON.parse(localStorage.play_audio)){
+		audio = new Audio('oof.mp3');
+		//localStorage.playback_rate = 1;
+		//audio.playbackRate = localStorage.playback_rate;
+		audio.volume = 0.3;
+		audio.play();
+	}
+})
